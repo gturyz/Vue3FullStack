d7a838985fbf4be304207f4607c2ab838d042edb
Léo Bonhême--Morsent <gturyz@gmail.com>
2022-06-20 16:26:25 +0200

    [Q6] Ajout condition sur les routes

--- src/router.js
+++ src/router.js
@@ -4,6 +4,8 @@ import AccueilVue from "./components/Accueil.vue";
 import ConnexionVue from "./components/Connexion.vue";
 import InscriptionVue from "./components/Inscription.vue";
 import AboutVue from "./components/About.vue";
+import HasardVue from "./components/Hasard.vue";
+import { useAuthStore } from "./stores";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -14,7 +16,20 @@ const router = createRouter({
     { path: "/connexion", name: "Connexion", component: ConnexionVue },
     { path: "/inscription", name: "Inscription", component: InscriptionVue },
     { path: "/list", name: "List", component: ListVue },
+    { path: "/hasard", name: "Hasard", component: HasardVue },
   ],
+});
+
+router.beforeEach(async (to) => {
+  // redirect to login page if not logged in and trying to access a restricted page
+  const publicPages = ["/about", "/", "/hasard", "/connexion", "/inscription"];
+  const authRequired = !publicPages.includes(to.path);
+  const auth = useAuthStore();
+
+  if (authRequired && !auth.user) {
+    auth.returnUrl = to.fullPath;
+    return "/connexion";
+  }
 });
 
 export default router;
