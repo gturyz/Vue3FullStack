275d03862fbaae61f3613de99efe3bcca426d659
Léo Bonhême--Morsent <gturyz@gmail.com>
2022-06-20 14:43:31 +0200

    [Q1] Initialisation router && début Navbar

---
+++ src/components/About.vue
@@ -0,0 +1,3 @@
+<script setup></script>
+
+<template><h1>A propos</h1></template>
---
+++ src/components/Inscription.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from "vue";
+
+const email = ref("");
+const password = ref("");
+</script>
+
+<template>
+  <h1>Inscription</h1>
+  <form>
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Email address</label>
+      <input
+        type="email"
+        class="form-control"
+        id="exampleInputEmail1"
+        aria-describedby="emailHelp"
+        v-model="email"
+      />
+    </div>
+    <div class="mb-3">
+      <label for="exampleInputPassword1" class="form-label">Password</label>
+      <input
+        type="password"
+        class="form-control"
+        id="exampleInputPassword1"
+        v-model="password"
+      />
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+</template>
